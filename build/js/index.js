f29db0bfcf3b17aeb10109ce6b8c192f71a9017f
frozenfung <frozenfung@gmail.com>
2019-04-17 19:42:06 +0800

    fix: typo

---
+++ build/js/index.js
@@ -0,0 +1,56 @@
+if (window.innerWidth < 992) {
+  location.pathname = 'nosupport.html';
+}
+
+// init nanobar
+var nanobar;
+var options = {
+  target: document.querySelector('#preload-page'),
+};
+nanobar = new Nanobar(options);
+nanobar.go(20);
+
+window.addEventListener("load", function(event) {
+  // console.log("All resources finished loading!");
+  // fullpage init
+  $('#fullpage').fullpage({
+    navigation: true,
+    sectionsColor: ['whitesmoke', 'whitesmoke', 'whitesmoke', 'whitesmoke'],
+    onLeave: function(index) {
+      // console.log(index)
+      if (index == 2) {
+        $('.foodudes>*').removeClass('reveal');
+      }
+      if (index == 3) {
+        $('.icook>*').removeClass('reveal');
+      }
+      if (index == 4) {
+        $('.cakeresume>*').removeClass('reveal');
+      }
+      if (index == 5) {
+        $('.dmmt>*').removeClass('reveal');
+      }
+      if (index == 6) {
+        $('.end>*').removeClass('reveal');
+      }
+    }
+  });
+  nanobar.go(100);
+  setTimeout(function() {
+    document.querySelector('#fullpage').classList.add("show");
+    document.querySelector('#fp-nav ul').classList.add("show");
+    document.querySelector('#preload-page').classList.add("hidden");
+  }, 500);
+
+  // tooltips
+  tippy('[title]', {
+    placement: 'top',
+    theme: 'orange',
+    arrow: true
+  })
+});
+
+window.addEventListener("DOMContentLoaded", function(event) {
+  // console.log("DOM fully loaded and parsed");
+  nanobar.go(60);
+});
--- pages/nosupport.md
+++ pages/nosupport.md
@@ -8,6 +8,6 @@
       <span class="snore-2">z</span>
       <img src="images/nosupport/sleeping-meerkat.png">
     </div>
-    <p>Shhhhh~~~~<br/><br/>Meerkat is sleeping because he does not serve the  browser or device you are using. Please use a modern broswer with computer and visit him again.</p>
+    <p>Shhhhhhhhh~~~~<br/><br/>Meerkat is sleeping because he does not serve the  browser or device you are using. Please use a modern broswer with computer and visit him again.</p>
   </div>
 </div>
